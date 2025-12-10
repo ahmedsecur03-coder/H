9ef8f8856a318ecae257073b3d6094a2b9d56e78
Hagaaty <ahmedsecur03@gmail.com>
2025-12-10 20:24:13 +0000

    استبدل الخطاء
    1/1
    
    Next.js 15.5.7 (outdated)
    Turbopack
    Console Error

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -572,10 +572,8 @@ const SidebarMenuButton = React.forwardRef<
         className={cn(sidebarMenuButtonVariants({ variant, size }), className)}
         {...props}
       >
-        <React.Fragment>
-            {children}
-            {isCollapsible && <span className="sr-only">{typeof tooltip === 'string' ? tooltip : (tooltip as any)?.children}</span>}
-        </React.Fragment>
+        {children}
+        {isCollapsible && <span className="sr-only">{typeof tooltip === 'string' ? tooltip : (tooltip as any)?.children}</span>}
       </Comp>
     );
 
