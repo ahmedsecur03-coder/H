26d816051ed8ada881d083e804bb7928075bcae9
Hagaaty <ahmedsecur03@gmail.com>
2025-12-10 15:18:01 +0000

    أحتاج مساعدتك في بناء تطبيق متكامل يسمى "حاجاتي" - منصة خدمات رقمية شامل

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,7 +35,7 @@ export default function RootLayout({
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;600;700&family=PT+Sans:wght@400;700&display=swap" rel="stylesheet" />
       </head>
-      <body className={cn('font-body antialiased', fontPoppins.variable, fontPTSans.variable)}>
+      <body className={cn('font-body antialiased bg-background', fontPoppins.variable, fontPTSans.variable)}>
         <FirebaseClientProvider>
           {children}
           <AiAssistant />
