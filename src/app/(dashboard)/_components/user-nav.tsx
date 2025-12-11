85e25f0412a63ea101a94d564df2e1bd74105dc9
Hagaaty <ahmedsecur03@gmail.com>
2025-12-11 18:55:55 +0000

    Try fixing this error: `Console TypeError: Cannot read properties of und

--- src/app/(dashboard)/_components/user-nav.tsx
+++ src/app/(dashboard)/_components/user-nav.tsx
@@ -58,7 +58,7 @@ export function UserNav({ user, isAdmin = false }: UserNavProps) {
         <DropdownMenuLabel className="font-normal">
             <div className="flex flex-col space-y-1">
                 <p className="text-sm font-medium leading-none">{user.name}</p>
-                 <p className="text-xs text-muted-foreground font-mono">#{user.id.substring(0,8)}</p>
+                 {user.id && <p className="text-xs text-muted-foreground font-mono">#{user.id.substring(0,8)}</p>}
                 <p className="text-xs leading-none text-muted-foreground">
                 {user.email}
                 </p>
