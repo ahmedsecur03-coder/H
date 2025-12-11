468948e77574e36e224736daabc3e4c59d2895fa
Hagaaty <ahmedsecur03@gmail.com>
2025-12-11 18:09:08 +0000

    خليه فيه سحب كونية وكواكب ونيازك ونجوم وشمس وقمر و الارض والمجموعه الشمس

--- src/components/cosmic-background.tsx
+++ src/components/cosmic-background.tsx
@@ -6,15 +6,31 @@ import { cn } from "@/lib/utils";
 export default function CosmicBackground({ className }: { className?: string }) {
     return (
         <div className={cn("absolute inset-0 -z-10 h-full w-full bg-background overflow-hidden", className)}>
+            {/* Stars Layers */}
             <div id='stars' />
             <div id='stars2' />
             <div id='stars3' />
-            <div className="absolute inset-x-0 bottom-0 z-10 h-1/2 bg-gradient-to-t from-background to-transparent"></div>
+
+            {/* Solar System Elements */}
+            <div className="solar-system">
+                <div id="sun"></div>
+                <div id="earth-orbit">
+                    <div id="earth">
+                        <div id="moon-orbit">
+                            <div id="moon"></div>
+                        </div>
+                    </div>
+                </div>
+                 <div id="mars-orbit">
+                    <div id="mars"></div>
+                </div>
+            </div>
+
+            {/* Shooting Stars */}
             <div className="shooting-star"></div>
             <div className="shooting-star"></div>
             <div className="shooting-star"></div>
             <div className="shooting-star"></div>
-             <div className="absolute -bottom-1/2 left-1/2 -z-10 h-[200%] w-[200%] -translate-x-1/2 animate-move-background rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,hsl(var(--primary)/0.15),hsl(var(--background))_40%,hsl(var(--accent)/0.1)_80%,hsl(var(--background)))]"></div>
         </div>
     )
 }
