9dbe3f3d782e01257d108a3b9d458d6cc7671bbe
Hagaaty <ahmedsecur03@gmail.com>
2025-12-11 18:36:17 +0000

    استبدل الاشكال بنجور لامعه ومجرات بعيده لامعه و احتفالات خارج حدود الارض

--- src/components/cosmic-background.tsx
+++ src/components/cosmic-background.tsx
@@ -3,15 +3,17 @@
 
 import { cn } from "@/lib/utils";
 
+// This component renders a dynamic cosmic background with multiple layers of stars and shooting stars.
+// The styling and animations are handled purely by CSS in globals.css.
 export default function CosmicBackground({ className }: { className?: string }) {
     return (
         <div className={cn("aurora-background absolute inset-0 h-full w-full bg-background overflow-hidden", className)}>
-            {/* Stars Layers */}
-            <div id='stars' />
-            <div id='stars2' />
-            <div id='stars3' />
+            {/* These divs generate the star layers using CSS box-shadows. */}
+            <div className="stars-small"></div>
+            <div className="stars-medium"></div>
+            <div className="stars-large"></div>
 
-            {/* Shooting Stars */}
+            {/* These divs create the shooting star animations. */}
             <div className="shooting-star"></div>
             <div className="shooting-star"></div>
             <div className="shooting-star"></div>
