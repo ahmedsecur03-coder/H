ff5e983c2e9fb7a55e5d70412c88b1faa69be030
Hagaaty <ahmedsecur03@gmail.com>
2025-12-11 18:12:20 +0000

    لا خلاص رجعها زي الاول وخلاص خليها كون خارجي

--- src/components/cosmic-background.tsx
+++ src/components/cosmic-background.tsx
@@ -11,21 +11,6 @@ export default function CosmicBackground({ className }: { className?: string }) 
             <div id='stars2' />
             <div id='stars3' />
 
-            {/* Solar System Elements */}
-            <div className="solar-system">
-                <div id="sun"></div>
-                <div id="earth-orbit">
-                    <div id="earth">
-                        <div id="moon-orbit">
-                            <div id="moon"></div>
-                        </div>
-                    </div>
-                </div>
-                 <div id="mars-orbit">
-                    <div id="mars"></div>
-                </div>
-            </div>
-
             {/* Shooting Stars */}
             <div className="shooting-star"></div>
             <div className="shooting-star"></div>
