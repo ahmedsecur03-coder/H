d63b16acc6c8b8a997ea656d409047f16063b3d8
Hagaaty <ahmedsecur03@gmail.com>
2025-12-29 13:23:59 +0000

    رقي كمان عايز ترقيه 10 تجوم وابني بقيت صفحات الواجهه الرئيسيه علشان يبقي

--- src/app/agency-accounts/layout.tsx
+++ src/app/agency-accounts/layout.tsx
@@ -4,5 +4,4 @@ import PublicLayout from '../public-layout';
 export default function Layout({ children }: { children: React.ReactNode }) {
   return <PublicLayout>{children}</PublicLayout>;
 }
-
     
